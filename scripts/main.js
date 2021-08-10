53d2a6b4a2e52aa1ec86754a0c89946b3f21006e
Brent Lee Johnson <brentleejohnson73@gmail.com>
2021-08-10 16:29:24 +0200

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+let width = getComputedStyle(document.body).width;
+let height = getComputedStyle(document.body).height;
+
+let logContainer = document.querySelector(".login-img");
+
+logContainer.style.width = width;
+logContainer.style.height = height;
