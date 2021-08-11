6ebfc80745216a1a18c06c6d45492ff3b47e4c60
Brent Lee Johnson <brentleejohnson73@gmail.com>
2021-08-11 13:36:37 +0200

    Added active class to the tabs

--- scripts/main.js
+++ scripts/main.js
@@ -5,3 +5,18 @@
 
 // logContainer.style.width = width;
 // logContainer.style.height = height;
+
+// Active class function
+// for login/register buttons
+let buttons = document.querySelectorAll(".tab");
+
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", change);
+}
+
+function change(e) {
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].classList.remove("active");
+  }
+  e.currentTarget.classList.add("active");
+}
