0f94eca52a161834cc9557220e41f271c4948ae7
Brent Lee Johnson <brentleejohnson73@gmail.com>
2021-08-12 16:21:47 +0200

    Preparing to add products

--- scripts/index.js
+++ scripts/index.js
@@ -42,3 +42,74 @@ register.addEventListener("click", () => {
   register.classList.add("active");
   reg_form.classList.add("active");
 });
+
+// LOGIN
+function userLogin(username, password) {
+  console.log(username);
+  console.log(password);
+  fetch("https://secure-harbor-41796.herokuapp.com/auth", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username: username,
+      password: password,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data["access_token"]) {
+        console.log(data);
+        myStorage = window.localStorage;
+        myStorage.setItem("jwt-token", data["access_token"]);
+        myStorage.setItem("username", username);
+        myStorage.setItem("password", password);
+        // window.location.href = "./products.html";
+      }
+    });
+}
+
+login_form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let username = document.querySelector("#username").value;
+  let password = document.querySelector("#password").value;
+  console.log(username);
+  console.log(password);
+
+  userLogin(username, password);
+});
+
+// REGISTER
+// function register(firstName, lastName, email, username, password) {
+//   console.log(firstName, lastName, email, username, password);
+//   fetch("https://agile-tundra-03577.herokuapp.com/register/", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify({
+//       first_name: firstName,
+//       last_name: lastName,
+//       email: email,
+//       username: username,
+//       password: password,
+//     }),
+//   })
+//     .then((response) => response.json())
+//     .then((data) => {
+//       console.log(data);
+//     });
+// }
+
+// switch between register and sign in
+// let switchButtons = document.querySelectorAll(".switchButton");
+// let signInForm = document.querySelector(".signIn");
+// let registerForm = document.querySelector(".register");
+
+// switchButtons.forEach((button) => {
+//   button.addEventListener("click", () => {
+//     signInForm.classList.toggle("active");
+//     registerForm.classList.toggle("active");
+//   });
+// });
