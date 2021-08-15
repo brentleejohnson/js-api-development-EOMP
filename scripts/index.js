d28681a064443efa9568d0a030b7c80e58af61c1
Brent Johnson <brentleejohnson73@gmail.com>
2021-08-15 15:38:21 +0200

    Tweaked login/register functions

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,3 @@
-// let width = getComputedStyle(document.body).width;
-// let height = getComputedStyle(document.body).height;
-
-// let logContainer = document.querySelector(".login-img");
-
-// logContainer.style.width = width;
-// logContainer.style.height = height;
-
 // Active class function
 // for login/register buttons
 let buttons = document.querySelectorAll(".tab");
@@ -15,17 +7,6 @@ let login_form = document.querySelector(".login-form");
 
 let login = document.querySelector("#login");
 let register = document.querySelector("#register");
-
-// for (let i = 0; i < buttons.length; i++) {
-//   buttons[i].addEventListener("click", change);
-// }
-
-// function change(e) {
-//   for (let i = 0; i < buttons.length; i++) {
-//     buttons[i].classList.remove("active");
-//   }
-//   e.currentTarget.classList.add("active");
-// }
 
 login.addEventListener("click", () => {
   register.classList.remove("active");
@@ -44,7 +25,11 @@ register.addEventListener("click", () => {
 });
 
 // LOGIN
-function userLogin(username, password) {
+let date = new Date();
+
+let logInButton = document.querySelector(".login");
+
+function loginF(username, password) {
   console.log(username);
   console.log(password);
   fetch("https://secure-harbor-41796.herokuapp.com/auth", {
@@ -65,51 +50,28 @@ function userLogin(username, password) {
         myStorage.setItem("jwt-token", data["access_token"]);
         myStorage.setItem("username", username);
         myStorage.setItem("password", password);
-        // window.location.href = "./products.html";
+        window.location.href = "/products.html";
       }
     });
 }
 
-login_form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  let username = document.querySelector("#username").value;
-  let password = document.querySelector("#password").value;
-  console.log(username);
-  console.log(password);
-
-  userLogin(username, password);
-});
-
 // REGISTER
-// function register(firstName, lastName, email, username, password) {
-//   console.log(firstName, lastName, email, username, password);
-//   fetch("https://agile-tundra-03577.herokuapp.com/register/", {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify({
-//       first_name: firstName,
-//       last_name: lastName,
-//       email: email,
-//       username: username,
-//       password: password,
-//     }),
-//   })
-//     .then((response) => response.json())
-//     .then((data) => {
-//       console.log(data);
-//     });
-// }
-
-// switch between register and sign in
-// let switchButtons = document.querySelectorAll(".switchButton");
-// let signInForm = document.querySelector(".signIn");
-// let registerForm = document.querySelector(".register");
-
-// switchButtons.forEach((button) => {
-//   button.addEventListener("click", () => {
-//     signInForm.classList.toggle("active");
-//     registerForm.classList.toggle("active");
-//   });
-// });
+function registerF(name, email, username, password) {
+  console.log(name, email, username, password);
+  fetch("https://secure-harbor-41796.herokuapp.com/registration/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email,
+      username: username,
+      password: password,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    });
+}
